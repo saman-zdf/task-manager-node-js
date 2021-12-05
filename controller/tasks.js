77c147d90623bf3aea6efbdb6d1362f0ad9d689c
saman-zdf <samanfyz68@gmail.com>
2021-12-05 11:12:41 +0800

    all controller logic been added

--- controller/tasks.js
+++ controller/tasks.js
@@ -1,6 +1,11 @@
 const Task = require('../models/task');
-const getAllTasks = (req, res) => {
-  res.json({ success: true, data: ['task 1', 'task 2'] });
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 const createTask = async (req, res) => {
@@ -8,20 +13,54 @@ const createTask = async (req, res) => {
     const task = await Task.create(req.body);
     res.status(201).json({ task });
   } catch (error) {
-    res.json({ error: error.errors.name.message });
+    res.status(500).json({ error: error.message });
   }
 };
 
-const singleTask = (req, res) => {
-  res.json({ id: req.params.id });
+const singleTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      // we set errors here if the id has the same legth but with different character, we can send a custom error message
+      return res.status(404).json({ msg: `No taks with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    // if the actual syntax is not the same length of the original id, it will send a generic message
+    res.status(500).json({ error: error });
+  }
 };
 
-const updateTask = (req, res) => {
-  res.json({ id: req.params.id });
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findByIdAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No taks with id: ${taskID}` });
+    }
+    // defferent way of sending response
+    // res.status(200).json({ task });
+    res.status(200).json({ task: null, success: true });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 };
 
-const deleteTask = (req, res) => {
-  res.json({ id: req.params.id });
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findByIdAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).json({ msg: `No taks with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 };
 
 module.exports = {
